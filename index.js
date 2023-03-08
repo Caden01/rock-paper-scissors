0556d0a23c8c89af13df68b9b35021f2932050d0
Caden <caden.carlson01@gmail.com>
2023-03-08 12:22:22 -0800

    Created a function that finds out if the compouter or player won.

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 function getComputerChoice() {
-    let rock_paper_scissors = [];
-    rock_paper_scissors [0] = "Rock";
-    rock_paper_scissors [1] = "Paper";
-    rock_paper_scissors [2] = "Scissors";
+    const rock_paper_scissors = ["Rock", "Paper", "Scissors"];
     let result = rock_paper_scissors[Math.floor(Math.random() * rock_paper_scissors.length)]
     return result;
 
 }
 
-function rockPaperScissors(computerSelection, playerSelection) {
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice().toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "Draw!";
+    }
+    else if (
+        (playerSelection === "rock"  && computerSelection === "scissors") ||
+        (playerSelection === "paper"  && computerSelection === "rock") ||
+        (playerSelection === "scissors"  && computerSelection === "paper")
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (
+        (computerSelection === "rock"  && playerSelection === "scissors") ||
+        (computerSelection === "paper"  && playerSelection === "rock") ||
+        (computerSelection === "scissors"  && playerSelection === "paper")
+    ) {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    else {
+        return "Error, you must enter rock, paper or scissors"
+    }
+        
+        
+    
     
 }
 
 
 const playerSelection = prompt("Enter rock, paper or scissors");
+console.log(playerSelection);
+
 const computerSelection = getComputerChoice();
 
 console.log(getComputerChoice());
-console.log(rockPaperScissors());
+console.log(playRound(playerSelection, computerSelection));
